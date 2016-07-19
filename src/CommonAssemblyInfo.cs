bd22499849dc934af4a83bf774fcbcfa8117466a
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-07-19 11:40:11 +1000

    wip

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("Cake.Genymotion")]
 [assembly: AssemblyVersion("0.0.1")]
 [assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1.1468891024")]
+[assembly: AssemblyInformationalVersion("0.0.1.1468891943")]
 [assembly: AssemblyCopyright("Copyright (c) Geoffrey Huntley")]
 
